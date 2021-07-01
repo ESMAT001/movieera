faf79642cd9c0af7ffca62917b762ba3351b7aa7
ESMAT001 <esmat10.niazi@gmail.com>
2021-07-02 00:32:25 +0430

    pagenation component added

--- components/Pagenation.js
+++ components/Pagenation.js
@@ -1,13 +1,47 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Pagenation({ currentPage, endPage }) {
+
+    let endPageCount = currentPage + 3 > endPage ? currentPage + (endPage - currentPage) : currentPage + 3
+
     let pages = [];
-    for (let page = currentPage; page <= currentPage + 5; page++) pages.push(page);
+    for (let page = currentPage === 1 ? currentPage : currentPage - 1;
+        page <= endPageCount;
+        page++) pages.push(page);
+        
     return (
-        <ul>
-            {pages.map((page) => <li key={page}>{page}</li>)}
-            <li>...</li>
-            <li>{endPage}</li>
+        <ul className="flex m-5 space-x-2">
+            {currentPage !== 1 && currentPage - 1 !== 1 &&
+                <>
+                    <li className="bg-blue-400 text-white px-2 py-1">
+                        <Link href={"/movies/page/1"}>
+                            <a>1</a>
+                        </Link>
+                    </li>
+                    <li>...</li>
+                </>}
+
+
+            {pages.map((page) => <li
+                key={page}
+                className={(page === currentPage ? "bg-red-400" : "bg-blue-400") + " text-white px-2 py-1"}>
+                <Link href={"/movies/page/" + page}>
+                    <a>{page}</a>
+                </Link>
+            </li>)}
+
+            { !pages.includes(endPage) &&
+                <>
+                    <li>...</li>
+                    <li className="bg-blue-400 text-white px-2 py-1">
+                        <Link href={"/movies/page/" + endPage}>
+                            <a>{endPage}</a>
+                        </Link>
+                    </li>
+                </>}
+
+
         </ul>
     )
 }
