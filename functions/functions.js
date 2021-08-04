c190b258b466ab2598614483c5f92ca2ab8ff87d
ESMAT001 <esmat10.niazi@gmail.com>
2021-08-04 23:50:05 +0430

    origin added to req header for cors

--- functions/functions.js
+++ functions/functions.js
@@ -1,4 +1,8 @@
-export const callApi = async function (url, options = {}) {
+export const callApi = async function (url, options = {
+    headers: {
+        origin:"https://movieera.esmat@001.com"
+    },    
+}) {
     try {
         let data = await fetch(url, options)
         data = await data.json();
