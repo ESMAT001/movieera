9c6f0abc1864a136837f7379d91f22c517feea72
ESMAT001 <esmat10.niazi@gmail.com>
2021-08-05 20:14:43 +0430

    images bug fixed

--- utils.js
+++ utils.js
@@ -1,12 +1,16 @@
 module.exports = {
     apiUrl: "https://api-movieera.herokuapp.com/v1",
     imageUrl: (imgPath) => {
+        if(!imagePath) return "";
+
         if (imgPath[0] !== "/") {
             imgPath = "/" + imgPath;
         }
         return 'https://api-movieera.herokuapp.com/v1/image/original' + imgPath;
     },
     placeholderImgUrl: (imgPath) => {
+        if(!imagePath) return "";
+
         if (imgPath[0] !== "/") {
             imgPath = "/" + imgPath;
         }
