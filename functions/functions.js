c4e43403b9dfd7c6983266e1e0ce2585f1adfdbf
ESMAT001 <esmat10.niazi@gmail.com>
2021-08-05 10:20:49 +0430

    test push for cors test

--- functions/functions.js
+++ functions/functions.js
@@ -1,8 +1,4 @@
-export const callApi = async function (url, options = {
-    headers: {
-        origin:"https://movieera.esmat@001.com"
-    },    
-}) {
+export const callApi = async function (url, options = {}) {
     try {
         let data = await fetch(url, options)
         data = await data.json();
