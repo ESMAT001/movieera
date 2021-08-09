fce4de394ad6a15ae43516189297c0dad43444a7
ESMAT001 <esmat10.niazi@gmail.com>
2021-08-09 17:55:52 +0430

    on blur bug fixed

--- components/utils/HeartedMoviesContainer.js
+++ components/utils/HeartedMoviesContainer.js
@@ -3,21 +3,25 @@ import { BsHeart, BsFillHeartFill } from "react-icons/bs";
 import HeartedMovies from './HeartedMovies';
 function HeartedMoviesContainer() {
     const [isHeartBtnClicked, setHeartBtnClicked] = useState(false)
+    const [isMouseOver, setMouseOver] = useState(false)
     function handleHeartClick() {
         setHeartBtnClicked(prev => !prev)
     }
     return (
-        <div className="z-30 relative">
-            <button 
-            onBlur={handleHeartClick}
-            onClick={handleHeartClick} className="text-gray-300 text-xl  transition-all duration-300 cursor-pointer flex justify-end items-start">
+        <div
+            onMouseEnter={() => setMouseOver(true)}
+            onMouseLeave={() => setMouseOver(false)}
+            onBlur={() => { if (!isMouseOver) handleHeartClick() }}
+            className="z-30 relative">
+            <button
+                onClick={handleHeartClick} className="text-gray-300 text-xl  transition-all duration-300 cursor-pointer flex justify-end items-start">
                 {
                     isHeartBtnClicked ?
                         <BsFillHeartFill className="text-nice-red" /> :
                         <BsHeart />
                 }
             </button>
-            {isHeartBtnClicked && <HeartedMovies />}
+            {isHeartBtnClicked && <HeartedMovies close={handleHeartClick} />}
         </div>
     )
 }
