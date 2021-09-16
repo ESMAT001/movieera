f988656aa757fc494a42a0706dca5b8bed68cbd0
ESMAT001 <esmat10.niazi@gmail.com>
2021-09-16 14:49:35 +0430

    footer data added

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,45 @@ import { AiOutlineTwitter, AiOutlineInstagram } from "react-icons/ai";
 import { FaFacebookSquare } from "react-icons/fa";
 
 function Footer() {
-    
+    const links = [
+        "Action"
+        ,
+        "Adventure"
+        ,
+        "Animation"
+        ,
+        "Comedy"
+        ,
+        "Crime"
+        ,
+        "Documentary"
+        ,
+        "Drama"
+        ,
+        "Family"
+        ,
+        "Fantasy"
+        ,
+        "History"
+        ,
+        "Horror"
+        ,
+        "Music"
+        ,
+        "Mystery"
+        ,
+        "Romance"
+        ,
+        "Science Fiction"
+        ,
+        "TV Movie"
+        ,
+        "Thriller"
+        ,
+        "War"
+        ,
+        "Western"
+    ]
     return (
         <footer className="bg-black-dark flex flex-col space-y-20 px-10 py-16 sm:px-14 md:px-20 lg:px-32 xl:px-48 2xl:px-72" >
             <div className="flex flex-col items-center sm:items-start sm:justify-between space-y-16 sm:flex-row sm:space-y-0 ">
@@ -15,58 +53,23 @@ function Footer() {
                         </a>
                     </Link>
                 </div>
-                <div className="flex flex-col justify-center space-y-4 text-xs text-gray-500 text-opacity-90">
-                    <div className="flex justify-start space-x-10">
-                        <Link href="/">
-                            <a >
-                                Movies
-                            </a>
-                        </Link>
-                        <Link href="/">
-                            <a >
-                                Action
-                            </a>
-                        </Link>
-                        <Link href="/">
-                            <a >
-                                Movies hi
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="flex justify-start space-x-10">
-                        <Link href="/">
-                            <a >
-                                Movies
-                            </a>
-                        </Link>
-                        <Link href="/">
-                            <a >
-                                Action
-                            </a>
-                        </Link>
-                        <Link href="/">
-                            <a >
-                                Movies test
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="flex justify-start space-x-10">
-                        <Link href="/">
-                            <a >
-                                Movies
-                            </a>
-                        </Link>
-                        <Link href="/">
-                            <a >
-                                Action
-                            </a>
-                        </Link>
-                        <Link href="/">
-                            <a >
-                                MovieERA
-                            </a>
-                        </Link>
-                    </div>
+                <div className="grid grid-cols-4 justify-items-center text-center gap-5 text-xs text-gray-500 text-opacity-90">
+                    <Link href={`/movies`} key={-1 + "footer"}>
+                        <a >
+                            Latest
+                        </a>
+                    </Link>
+                    {
+                        links.map((link, index) => {
+                            return (
+                                <Link href={`/genre/${link}/page/1`} key={index + "footer"}>
+                                    <a >
+                                        {link}
+                                    </a>
+                                </Link>
+                            )
+                        })
+                    }
                 </div>
             </div>
             <div className="flex flex-col sm:flex-row justify-between space-y-4 sm:space-y-0 ">
