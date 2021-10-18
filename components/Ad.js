16b45abd1928864928ea1fd8e37713010e55c020
ESMAT001 <esmat10.niazi@gmail.com>
2021-10-18 19:20:32 +0430

    testing ad component

---
+++ components/Ad.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Ad extends Component {
+    componentDidMount() {
+        try {
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (error) {
+            console.log('ads load failed')
+        }
+    }
+    render() {
+        return (
+            <ins {...this.props}>
+            </ins>
+        );
+    }
+}
+
+export default Ad;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Media from '../components/Media'
 import CustomHead from '../components/utils/CustomHead'
 import CustomHeader from '../components/mainPageComponents/CustomHeader'
 import AdSense from 'react-ssr-adsense';
+import Ad from '../components/utils/Ad'
 import { apiUrl } from '../utils'
 
 
@@ -57,24 +58,25 @@ export default function Home({ movies, error }) {
       <Media movies={trending.slice(6)} error={error} topPadding={true} />
       {/* cool ads */}
       <div className="mb-12 px-10 sm:px-14 md:px-20 lg:px-32 xl:px-48  2xl:px-72 w-full mx-auto text-gray-200">
-        <AdSense
-          key="googleAd-1"
+        <Ad
+          key="googleAds-1"
+          className="adsbygoogle"
           style={{ display: "block", textAlign: "center" }}
-          layout="in-article"
-          format="fluid"
-          client="ca-pub-8407721631737964"
-          slot="8059491915"
+          data-ad-layout="in-article"
+          data-ad-format="fluid"
+          data-ad-client="ca-pub-8407721631737964"
+          data-ad-slot="8059491915"
         />
       </div>
       <div className="hidden md:block mb-12 px-10 sm:px-14 md:px-20 lg:px-32 xl:px-48  2xl:px-72 w-full mx-auto text-gray-200">
-        <AdSense
-          key="googleAd-2"
+        <Ad
+          key="googleAds-2"
           className="adsbygoogle"
           style={{ display: "block" }}
-          client="ca-pub-8407721631737964"
-          slot="3368426100"
-          format="auto"
-          responsive="true"
+          data-ad-client="ca-pub-8407721631737964"
+          data-ad-slot="3368426100"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
         />
       </div>
       {/*cool ad */}
